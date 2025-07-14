853540702c68b242d49da02426d52dc1e64221bc
Victor Hugo <victor.couto@ufrpe.br>
2025-07-14 14:12:08 -0300

    base do projeto concluida

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require ('express')
+const app = express();
+const bodyParser = require('body-parser')
+const connection = require('./database/database.js')
+
+//VIEW ENGINE
+app.set('view engine', 'ejs');
+//ARQUIVOS ESTÁTICOS
+app.use(express.static('public'))
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//DATABASE
+connection.authenticate().then(() => {
+    console.log('Conexão com o banco de dados feita com sucesso!')
+}).catch((error) => {
+    console.log(error);
+
+})
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(8080, () => {
+    console.log('O servidor está rodando');
+})
